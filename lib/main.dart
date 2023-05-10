3bb589e24e8b6beae96212399c3a3b5b2b7538e7
NwaeNandarAung97 <nandar.a@codetism.com>
2023-05-10 14:56:15 +0700

    responsive for landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
+// import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 93, 238, 127),
@@ -11,6 +12,9 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+  //     .then((fn) => {
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
@@ -52,4 +56,5 @@ void main() {
       home: const Expenses(),
     ),
   );
+  // });
 }
